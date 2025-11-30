69035acb072687d1a6a3c49e1b948fb494acd70d
Kelly <74309549+Kelechi-Iroegbu@users.noreply.github.com>
2025-11-30 20:01:53 +0100

    Update _app.tsx

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,10 +1,14 @@
+import "@/styles/global.css";
 import type { AppProps } from "next/app";
-import { CountProvider } from "../context/CountContext";
+import Layout from "@/components/layouts/Layout";
+import { CountProvider } from "@/context/CountContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CountProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </CountProvider>
   );
 }
